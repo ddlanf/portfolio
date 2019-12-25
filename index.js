6f5d5989dea6bc719c6de269cff84da1a6d22f9b
Daniel Laufenberg <danlanfen26@gmail.com>
2019-12-25 02:26:16 -1000

    Phone contents adjust added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function rotatePhone(){
         $(".rotate-bottom").css('display', `none`);
         setTimeout(displayTopButton, 600);
         phoneRotate = true;
+        setTimeout(reorganizeContent, 1000);
     });
 
     $(".rotate-top").click(function(){
@@ -14,6 +15,7 @@ function rotatePhone(){
         $(".rotate-top").css('display', `none`);    
         setTimeout(displayBottomButton, 600);
         phoneRotate = false;
+        setTimeout(reorganizeContent, 1000);
     });
 }
 
@@ -44,10 +46,101 @@ function deleteButtons(){
     });
 }
 
+//function to reorganize screen content
+function reorganizeContent(){
+    if(phoneRotate === true){
+        $(".my-pic").animate({
+            top: "300px",
+            left: "45px"
+        });
+        $(".my-pic").css('transform', `rotate(-90deg)`);
+
+        $(".bio-button").animate({
+            transform: "rotate(-90deg)",
+            top: "120px",
+            left: "0px"
+        });
+        $(".bio-button").css('transform', `rotate(-90deg)`);
+        
+        $(".portfolio-button").animate({
+            transform: "rotate(-90deg)",
+            top: "120px",
+            left: "120px"
+        });
+        $(".portfolio-button").css('transform', `rotate(-90deg)`);
+      
+    }
+
+    if(phoneRotate === false){
+        $(".my-pic").animate({
+            top: "60px",
+            left: "45px"
+        });
+        $(".my-pic").css('transform', `rotate(0deg)`);
+
+        $(".bio-button").animate({
+            transform: "rotate(0deg)",
+            top: "300px",
+            left: "60px"
+        });
+        $(".bio-button").css('transform', `rotate(0deg)`);
+        
+        $(".portfolio-button").animate({
+            transform: "rotate(-90deg)",
+            top: "430px",
+            left: "60px"
+        });
+        $(".portfolio-button").css('transform', `rotate(0deg)`);
+      
+    }
+
+}
+
+//These functinons allows the hover effect at 90 degrees
+function hoverEffectForBio(){
+    $(".bio-button").mouseenter( function(){
+        if(phoneRotate === true){
+            $(".bio-button").css({"top":"110px", "left": "-10px"});
+
+            $(".bio-button").mouseleave( function(){
+                $(".bio-button").css({"top":"120px", left:"0px"});
+            });
+        }
+        else{
+            $(".bio-button").css({"top":"290px", "left": "50px"});
+
+            $(".bio-button").mouseleave( function(){
+                $(".bio-button").css({"top":"300px", left:"60px"});
+            });
+        }
+    });
+}
+
+function hoverEffectForPortfolio(){
+    $(".portfolio-button").mouseenter( function(){
+        if(phoneRotate === true){
+            $(".portfolio-button").css({"top":"110px", left:"110px"});
+            
+            $(".portfolio-button").mouseleave( function(){
+                $(".portfolio-button").css({"top":"120px", left:"120px"});
+            });
+        }
+        else{
+            $(".portfolio-button").css({"top":"420px", left:"50px"});
+            $(".portfolio-button").mouseleave( function(){
+                $(".portfolio-button").css({"top":"430px", left:"60px"});
+            });
+        }
+    });
+}
+
+
 //function to run all functions
 function runAllfunctions(){
     rotatePhone();
     deleteButtons();
+    hoverEffectForBio();
+    hoverEffectForPortfolio();
 } 
 
 $(runAllfunctions);
